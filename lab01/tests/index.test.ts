db8f71d17fa4634dcd1948f73a320917bd7f0a64
Anton Zlygostev <zlygostev@hotmail.com>
2025-03-05 08:16:35 +0700

    Fixed the operation priority test

--- lab01/tests/index.test.ts
+++ lab01/tests/index.test.ts
@@ -24,8 +24,7 @@ describe('testing addition and multiplication', () => {
     test("Multiplication is a valid expression", 3, evaluate, 42, "6*7");
     test("Incomplete multiplication is invalid", 3, evaluate, SyntaxError, "42*");
     test("Standalone * is invalid", 3, evaluate, SyntaxError, "*42");
-    test("Multiplication is performed before addition", 4, evaluate, 42, "6*6+6");
-    test("Multiplication is performed before addition - 2", 4, evaluate, 42, "7+5*7");
+    test("Multiplication is performed before addition", 4, evaluate, 42, "7+4*7+7");
 
     test("Complex expressions are supported", 4, evaluate, 42, "7+2*7+3*6+3");
     test("Parentheses are correctly supported", 4, evaluate, 42, "(2+1)*7+(3*2*3)+2*(1+0)+1");
