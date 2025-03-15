3a2908471ebee8bfb48a6fe4f1f2040a8a1fb431
Anton Zlygostev <zlygostev@hotmail.com>
2025-03-15 14:22:01 +0700

    Implementing the deferred initialization for the expected results in tests; updating the tests for lab06

--- lab06/tests/index.test.ts
+++ lab06/tests/index.test.ts
@@ -1,6 +1,11 @@
-// import { compileFunction } from "../src";
 import { test } from "../../mark";
-import { deriveAndCalculate, deriveAndPrint } from "../src";
+
+import { Expr, parseExpr, printExpr } from "../../lab04";
+import { compileAndExecute, getVariables } from "../../lab05";
+
+import { derive } from "../src";
+
+const parseAndDerive = (source: string, varName: string) => derive(parseExpr(source), varName);
 
 
 describe("Numerical tests", ()=>{
@@ -18,17 +23,30 @@ describe("Numerical tests", ()=>{
 });
 
 describe("Structural tests", ()=>{
-    test("Constant derives to zero 1", 3, deriveAndPrint, "0", "42", "x");
-    test("Constant derives to zero 2", 4, deriveAndPrint, "0", "-42", "x");
-    test("Variable derives to one", 3, deriveAndPrint, "1", "x", "x");
-    test("Variable derives by other variable to zero", 3, deriveAndPrint, "0", "x", "y");
-    test("(f + g)' = f' + g' 1", 4, deriveAndPrint, "2 + y", "2*x+x*y", "x");
-    test("(f + g)' = f' + g' 2", 4, deriveAndPrint, "x", "2*x+x*y", "y");
-    test("(f - g)' = f' - g' 1", 4, deriveAndPrint, "2 - y", "2*x-x*y", "x");
-    test("(f - g)' = f' - g' 2", 4, deriveAndPrint, "-x", "2*x-x*y", "y");    
-    test("(f - g)' = f' - g' 3", 4, deriveAndPrint, "x", "x*y-x*2", "y");    
-    test("(f*g)' == f'*g + f*g' 1", 4, deriveAndPrint, "y", "x*y", "x");
-    test("(f*g)' == f'*g + f*g' 2", 4, deriveAndPrint, "x", "x*y", "y");
-    test("(f*g)' == f'*g + f*g' 3", 4, deriveAndPrint, "x * y * f * g * h", "x*y*z*f*g*h", "z");
-    test("(f / g)' = (f'*g - g'*f)/g*g ", 4, deriveAndPrint, "1 / (y * y)", "5*x-1/y", "y");
+    test("Constant derives to zero 1", 3, parseAndDerive, parseExpr("0"), "42", "x");
+    test("Constant derives to zero 2", 4, parseAndDerive, parseExpr("0"), "-42", "x");
+    test("Variable derives to one", 3, parseAndDerive, parseExpr("1"), "x", "x");
+    test("Variable derives by other variable to zero", 3, parseAndDerive, parseExpr("0"), "x", "y");
+    test("(f + g)' = f' + g' 1", 4, parseAndDerive, parseExpr("2+y"), "2*x+x*y", "x");
+    test("(f + g)' = f' + g' 2", 4, parseAndDerive, parseExpr("x"), "2*x+x*y", "y");
+    test("(f - g)' = f' - g' 1", 4, parseAndDerive, parseExpr("2-y"), "2*x-x*y", "x");
+    test("(f - g)' = f' - g' 2", 4, parseAndDerive, parseExpr("-x"), "2*x-x*y", "y");    
+    test("(f - g)' = f' - g' 3", 4, parseAndDerive, parseExpr("x"), "x*y-x*2", "y");    
+    test("(f*g)' == f'*g + f*g' 1", 4, parseAndDerive, parseExpr("y"), "x*y", "x");
+    test("(f*g)' == f'*g + f*g' 2", 4, parseAndDerive, parseExpr("x"), "x*y", "y");
+    test("(f*g)' == f'*g + f*g' 3", 4, parseAndDerive, parseExpr("x*y*f*g*h"), "x*y*z*f*g*h", "z");
+    test("(f / g)' = (f'*g - g'*f)/g*g ", 4, parseAndDerive, ()=>parseExpr("1/(y*y)"), "5*x-1/y", "y");
 });
+
+
+
+async function deriveAndCalculate(source: string, varName: string, ...args:number[]): Promise<number>
+{
+    let expr = parseExpr(source);
+    // we will use the original variable list to isolate the user from the 
+    // case when some variable(s) disappear after derivation
+    // e.g. x + x*y + z |'x yields (1 + y), leaving only one variable.
+    let variables = getVariables(expr); 
+    let derivative = derive(expr, varName);
+    return await compileAndExecute(derivative, variables, ...args);
+}
--- mark/src/index.ts
+++ mark/src/index.ts
@@ -7,17 +7,31 @@ export function test<T extends any[], R>(name: string, testMark: DesiredMark, fn
 {
     if (desiredMark >= testMark)
     {
-        if(typeof expected === "function") // Exception case
-            testJs(name, async () => await expect(async () => (await fn(...args))).rejects.toThrow(expected as ExceptionType));
+        if(typeof expected === "function") 
+        {
+            if(isConstructor(expected)) // exception case
+                testJs(name, async () => await expect(async () => (await fn(...args))).rejects.toThrow(expected));
+            else // deferred value case
+                testJs(name, async () => expect(await fn(...args)).toEqual((expected as Function)()));
+        }
         else
             testJs(name, async () => expect(await fn(...args)).toEqual(expected));
     }
     else
         testJs.skip(name, ()=>{});
 };
-
-type ResultOrException<R> = ExceptionType | Awaited<R>;
+type F<R> = ()=>R; 
+type ResultOrException<R> = ExceptionType | Awaited<R> | F<Awaited<R>>;
 
 interface ExceptionType {
     new(...args: any[]): any;
+}
+
+const handler={construct(){return handler}} //Must return ANY object, so reuse one
+const isConstructor= (x:any): x is ExceptionType => {
+    try{
+        return !!(new (new Proxy(x, handler))())
+    }catch(e){
+        return false
+    }
 }
