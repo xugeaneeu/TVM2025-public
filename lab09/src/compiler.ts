e08c9f98877e4e0c16b1c443bf9b748668e4cf6b
Anton Zlygostev <zlygostev@hotmail.com>
2025-03-20 21:26:22 +0700

    Refining lab09

--- lab09/src/compiler.ts
+++ lab09/src/compiler.ts
@@ -1,14 +1,13 @@
 import { writeFileSync } from "fs";
 import { Op, I32, Void, c, BufferedEmitter, LocalEntry} from "../../wasm";
-
-import { ResolvedModule } from "./resolver";
+import { Module } from "../../lab08";
 
 const { i32, 
     varuint32,
     get_local, local_entry, set_local, call, if_, void_block, void_loop, br_if, str_ascii, export_entry,
     func_type_m, function_body, type_section, function_section, export_section, code_section } = c;
   
-export async function compileModule(m: ResolvedModule): Promise<WebAssembly.Exports>
+export async function compileModule(m: Module): Promise<WebAssembly.Exports>
 {
     throw "Not implemented";
 }
