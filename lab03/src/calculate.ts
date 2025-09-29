7338c9eb5c1668596c1b8cf0b1d6a704d1fbe66e
xugeaneeu <daniilpuchkov59@gmail.com>
2025-09-29 18:51:47 +0700

    lab03 done

--- lab03/src/calculate.ts
+++ lab03/src/calculate.ts
@@ -5,6 +5,60 @@ export const arithSemantics: ArithSemantics = grammar.createSemantics() as Arith
 
 
 const arithCalc = {
+  Expr(arg0) {
+    return arg0.calculate(this.args.params);
+  },
+  AddSub(arg0, arg1, arg2) {
+    let result = arg0.calculate(this.args.params);
+    for (let i = 0; i < arg2.numChildren; i++) {
+      const node = arg2.child(i);
+      const value = node.calculate(this.args.params);
+      if (arg1.child(i).sourceString == "+") {
+        result += value;
+      } else {
+        result -= value;
+      }
+    }
+    return result;
+  },
+  DivMul(arg0, arg1, arg2) {
+    let result = arg0.calculate(this.args.params);
+    for (let i = 0; i < arg2.numChildren; i++) {
+      const node = arg2.child(i);
+      const value = node.calculate(this.args.params); 
+      if (arg1.child(i).sourceString == "*") {
+        result *= value;
+      } else {
+        if (value == 0) {
+          throw new Error("division by zero");
+        }
+        result /= value
+      }
+    }
+    return result;
+  },
+  Prim_unmin(_arg0, arg1) {
+    return -arg1.calculate(this.args.params);
+  },
+  Prim_num(arg0) {
+    return arg0.calculate(this.args.params);
+  },
+  Prim_var(arg0) {
+    return arg0.calculate(this.args.params);
+  },
+  Prim_par(_arg0, arg1, _arg2) {
+    return arg1.calculate(this.args.params);
+  },
+  var(_arg0, _arg1) {
+    const name = this.sourceString;
+    if (!(name in this.args.params!)) {
+      return NaN;
+    }
+    return this.args.params![name];
+  },
+  number(_arg0) {
+    return parseInt(this.sourceString, 10);
+  },
 } satisfies ArithmeticActionDict<number | undefined>;
 
 
@@ -12,10 +66,10 @@ arithSemantics.addOperation<Number>("calculate(params)", arithCalc);
 
 
 export interface ArithActions {
-    calculate(params: {[name:string]:number}): number;
+  calculate(params: {[name:string]:number}): number;
 }
 
 export interface ArithSemantics extends ArithmeticSemantics
 {
-    (match: MatchResult): ArithActions;
+  (match: MatchResult): ArithActions;
 }
--- lab03/src/index.ts
+++ lab03/src/index.ts
@@ -7,18 +7,23 @@ export {ArithmeticActionDict, ArithmeticSemantics} from './arith.ohm-bundle';
 
 export function evaluate(content: string, params?: {[name:string]:number}): number
 {
-    return calculate(parse(content), params ?? {});
+  return calculate(parse(content), params ?? {});
 }
+
 export class SyntaxError extends Error
 {
 }
 
 export function parse(content: string): MatchResult
 {
-    throw "Not implemented";
+  const match = grammar.match(content);
+  if (match.failed()) {
+    throw new SyntaxError(match.message);
+  }
+  return match;
 }
 
 function calculate(expression: MatchResult, params: {[name:string]: number}): number
 {
-    throw "Not implemented";
+  return arithSemantics(expression).calculate(params);
 }
