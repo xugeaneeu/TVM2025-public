3f39c999a116c8dd41d4b1d17c654bdab2e8faaf
xugeaneeu <daniilpuchkov59@gmail.com>
2025-10-26 20:28:22 +0700

    lab05 done

--- lab05/src/compiler.ts
+++ lab05/src/compiler.ts
@@ -3,16 +3,59 @@ import { Expr } from "../../lab04";
 import { buildOneFunctionModule, Fn } from "./emitHelper";
 const { i32, get_local} = C;
     
+export function traverse(node: Expr, vi: Set<String>, vars: string[]) {
+  switch (node.type) {
+    case "num":return;
+    case "var":
+      if (!vi.has(node.name)) {
+        vi.add(node.name);
+        vars.push(node.name);
+      }
+      return;
+    case "neg":
+      traverse(node.arg, vi, vars);
+      return;
+    case "bin":
+      traverse(node.left, vi, vars);
+      traverse(node.right, vi, vars);
+      return;
+  }
+}
+
 export function getVariables(e: Expr): string[] {
-    throw "Not implemented";
+  const visited = new Set<string>();
+  const variables: string[] = [];
+
+  traverse(e, visited, variables);
+  return variables;
 }
 
 export async function buildFunction(e: Expr, variables: string[]): Promise<Fn<number>>
 {
-    let expr = wasm(e, variables)
-    return await buildOneFunctionModule("test", variables.length, [expr]);
+  let expr = wasm(e, variables)
+  return await buildOneFunctionModule("test", variables.length, [expr]);
 }
 
 function wasm(e: Expr, args: string[]): Op<I32> {
-    throw "Not implemented";
+  switch (e.type) {
+    case "num":
+      return i32.const(e.value);
+    case "var":
+      const idx = args.indexOf(e.name);
+      if (idx < 0) {
+        throw new Error(`Unknown variable ${e.name}`);
+      }
+      return get_local(i32, idx);
+    case "neg":
+      return i32.sub(i32.const(0), wasm(e.arg, args));
+    case "bin":
+      const leftOp  = wasm(e.left,  args);
+      const rightOp = wasm(e.right, args);
+      switch (e.op) {
+        case "+": return i32.add(leftOp,  rightOp);
+        case "-": return i32.sub(leftOp,  rightOp);
+        case "*": return i32.mul(leftOp,  rightOp);
+        case "/": return i32.div_s(leftOp, rightOp);
+      }
+  }
 }
--- lab05/src/index.ts
+++ lab05/src/index.ts
@@ -2,24 +2,25 @@ import { Expr, parseExpr } from "../../lab04";
 import { buildFunction, getVariables } from "./compiler";
 import { Fn } from "./emitHelper";
 
-export async function parseCompileAndExecute(expression: string, ...args: number[]): Promise<number> {
-    let expr = parseExpr(expression);
-    let variables = getVariables(expr);
-    return await compileAndExecute(expr, variables, ...args);
+export async function parseCompileAndExecute(expression: string, ...args: number[]): Promise<number>
+{
+  let expr = parseExpr(expression);
+  let variables = getVariables(expr);
+  return await compileAndExecute(expr, variables, ...args);
 }
 export async function compileAndExecute(expr: Expr, variables: string[], ...args: number[]): Promise<number>
 {
-    let wasmFunc = await compile(expr, variables);
-    return wasmFunc(...args);
+  let wasmFunc = await compile(expr, variables);
+  return wasmFunc(...args);
 }
 
 export const compile = async (expr: Expr, variables: string[]) => checked(await buildFunction(expr, variables));
 
 export const checked = <R>(func: Fn<R>): Fn<R> => function(...args: number[]): R
-    {
-        if(args.length != func.length)
-            throw new WebAssembly.RuntimeError(`Signature mismatch: passed ${args.length}, expected ${func.length}.`);
-        return func(...args);
-    };
+{
+  if(args.length != func.length)
+    throw new WebAssembly.RuntimeError(`Signature mismatch: passed ${args.length}, expected ${func.length}.`);
+  return func(...args);
+};
 
 export { buildFunction, getVariables } from "./compiler";
